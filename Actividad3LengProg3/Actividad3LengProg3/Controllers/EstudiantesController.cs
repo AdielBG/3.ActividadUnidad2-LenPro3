014318a09e2c51d5e4d1d49735c10056767f5d41
AdielBG <adielbg17.2@gmail.com>
2025-06-08 12:11:42 -0400

    Controlador Estudiantes y Vista Index

---
+++ Actividad3LengProg3/Actividad3LengProg3/Controllers/EstudiantesController.cs
@@ -0,0 +1,58 @@
+﻿using Actividad3LengProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad3LengProg3.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        public static List<EstudianteViewModel> estudiantes = new List<EstudianteViewModel>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Registrar(EstudianteViewModel estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                estudiantes.Add(estudiante);
+                TempData["Mensaje"] = "Estudiante registrado exitosamente.";
+                //return RedirectToAction("Lista");
+            }
+            return View("Index", estudiante);
+        }
+
+
+        public IActionResult Lista() { 
+        
+            return View(estudiantes);
+        
+        }
+
+
+        public IActionResult Editar(string matricula)
+        {
+
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Editar(EstudianteViewModel model)
+        {
+            return View();
+        }
+
+
+        public IActionResult Eliminar(string matricula)
+        {
+
+            return View();
+
+        }
+
+    }
+}
