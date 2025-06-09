a391300c06e0489b00e880c51db0a92522497655
AdielBG <adielbg17.2@gmail.com>
2025-06-09 13:29:49 -0400

    Acción y Vista Editar Estudiante

--- Actividad3LengProg3/Actividad3LengProg3/Controllers/EstudiantesController.cs
+++ Actividad3LengProg3/Actividad3LengProg3/Controllers/EstudiantesController.cs
@@ -1,5 +1,6 @@
 ﻿using Actividad3LengProg3.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace Actividad3LengProg3.Controllers
 {
@@ -20,8 +21,10 @@ namespace Actividad3LengProg3.Controllers
                 estudiantes.Add(estudiante);
                 TempData["Mensaje"] = "Estudiante registrado exitosamente.";
                 return RedirectToAction("Lista");
+                
             }
-            return View("Index", estudiante);
+           
+                return View();
         }
 
 
@@ -31,20 +34,52 @@ namespace Actividad3LengProg3.Controllers
         
         }
 
-
+        [HttpGet]
         public IActionResult Editar(string matricula)
         {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante == null) {
+                TempData["Mensaje"] = "No existe el usuario indicado";
+                return RedirectToAction("Lista");
+            }
 
-            return View();
-
+            return View(estudiante);
         }
+
 
 
         [HttpPost]
-        public IActionResult Editar(EstudianteViewModel model)
+        public IActionResult Editar(EstudianteViewModel estudiante)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                EstudianteViewModel original = estudiantes.FirstOrDefault(e => e.Matricula.Equals(estudiante.Matricula));
+
+                if (original == null)
+                {
+                    TempData["Mensaje"] = "No existe el usuario indicado";
+                    return RedirectToAction("Lista");
+                }
+
+                // Actualiza los campos necesarios
+                original.NombreCompleto = estudiante.NombreCompleto;
+                original.Carrera = estudiante.Carrera;
+                original.CorreoInstitucional = estudiante.CorreoInstitucional;
+                original.Telefono = estudiante.Telefono;
+                original.FechaNacimiento = estudiante.FechaNacimiento;
+                original.Genero = estudiante.Genero;
+                original.Turno = estudiante.Turno;
+                original.TipoIngreso = estudiante.TipoIngreso;
+                original.PorcentajeBeca = estudiante.PorcentajeBeca;
+                original.TerminosYCondiciones = estudiante.TerminosYCondiciones;
+
+                TempData["Mensaje"] = "Actualizaciones realizadas correctamente";
+                return RedirectToAction("Lista");
+            }
+
+            return View(estudiante);
         }
+
 
 
         public IActionResult Eliminar(string matricula)
